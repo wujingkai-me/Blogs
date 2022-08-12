72c7aae251ab0b635801f70c177c5b93c256271e
presonal@aliyun.com <presonal@aliyun.com>
2022-08-12 19:16:56 +0800

    关闭语法检查

--- vue-cil-folder/src/main.js
+++ vue-cil-folder/src/main.js
@@ -3,8 +3,11 @@ import Vue from 'vue'
 // Vue是所有组件的父组件
 import App from './App.vue'
 
+let a = 0;
 // 关闭生产环境的提示
 Vue.config.productionTip = false
+
+// let a = 1;
 
 new Vue({
   render: h => h(App),
